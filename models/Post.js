73c929c955f2341f1512d9b72427d51b8f41aec0
Tricia Buckenberger <tbuckenberger@gmail.com>
2015-02-23 11:34:17 -0800

    starting Post Schema

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var postSchema = new mongoose.Schema({
+  storyID: String,
+  author: String,
+  segmentNumber: String,
+  postBody: String
+});
+
+module.exports = mongoose.model('Post', makeupSchema);
