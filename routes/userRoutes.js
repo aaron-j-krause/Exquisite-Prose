701cc5e27471bda30aaed091e83c14cd589289b4
rahul1346 <rahulsharma2001@gmail.com>
2015-02-23 17:39:21 -0800

    updating userRoutes file

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var bodyparser = require('body-parser');
+var User = require('../models/User');
+
+module.exports = function(app) {
+    app.use(bodyparser.json());
+    app.post('/create_user', function (req, res) {
+        var newUser = new User();
+        newUser
+
+
