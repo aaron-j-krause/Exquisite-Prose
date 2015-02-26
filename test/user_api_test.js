01ae3952e3c6e71dc17fcb1ad685939a74873741
Aaron Krause <aaronjkrause@gmail.com>
2015-02-26 01:58:17 -0800

    added test for other user endpoint

--- test/user_api_test.js
+++ test/user_api_test.js
@@ -10,25 +10,48 @@ chai.use(chaihttp);
 var expect = chai.expect;
 
 describe('user api end point', function() {
-    after(function(done) {
-        mongoose.connection.db.dropDatabase(function() {
-            done();
-        });
-     });
+  before(function(done){
+    chai.request('localhost:3000')
+      .post('/user/create_user')
+      .send({email: 'example@email.com', password: '1234abc',
+        screenname: 'exampleUser', location: 'examplion'})
+      .end(function(err, res){
+        done();
+      })
+  })
 
-     it('should respond to a post request to create_user', function(done){
-         chai.request('localhost:3000/user')
-            .post('/create_user')
-            .send({email: 'test@email.com', password: '1234abc',
-            screenname: 'testUser', location: 'Washington'})
-            .end(function(err, res) {
-                expect(err).to.eql(null);
-                expect(res.body.createdAt).to.not.equal(null);
-                expect(res.body.basic.email).to.eql('test@email.com');
-                expect(res.body.basic.password).to.eql('1234abc');
-                expect(res.body.screenname).to.eql('testUser');
-                expect(res.body.location).to.eql('Washington');
-                done();
-            });
-     });
+  after(function(done) {
+    mongoose.connection.db.dropDatabase(function() {
+      done();
+    });
+  });
+
+  it('should respond to a post request to create_user', function(done){
+   chai.request('localhost:3000/user')
+      .post('/create_user')
+      .send({email: 'test@email.com', password: '1234abc',
+        screenname: 'testUser', location: 'Washington'})
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.body.createdAt).to.not.equal(null);
+        expect(res.body.basic.email).to.eql('test@email.com');
+        expect(res.body.basic.password).to.eql('1234abc');
+        expect(res.body.screenname).to.eql('testUser');
+        expect(res.body.location).to.eql('Washington');
+        done();
+      });
+  });
+  it('should find a user by screenname', function(done){
+    chai.request('localhost:3000')
+      .get('/user/exampleUser')
+      .end(function(err, res){
+        var user = res.body.user;
+        var segments = res.body.segments;
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(user.screenname).to.eql('exampleUser');
+        expect(segments).to.be.empty;
+        done()
+      })
+  })
 });
